cb86a8706c97b7a183e659b115fc99d859d8415f
Kevin Wohlwend <kevin.wohlwend@gmail.com>
2026-04-18 12:34:06 -0600

    Update pause message font size in invaders.py

--- invaders.py
+++ invaders.py
@@ -544,10 +544,11 @@ def draw_paused(screen, font):
     overlay.set_alpha(180)
     overlay.fill(BLACK)
     screen.blit(overlay, (0, 0))
-
-    paused_msg1 = font.render("GAME PAUSED", True, (252, 3, 123))
+    big_font = pygame.font.Font(None, 100) 
+    paused_msg1 = big_font.render("GAME PAUSED", True, (252, 3, 123))
     paused_msg2 = font.render("Press the Dude button again to resume!", True, GREEN)
-    screen.blit(msg, (SCREEN_W//2 - msg.get_width()//2, SCREEN_H//2))
+    screen.blit(paused_msg1, (SCREEN_W//2 - paused_msg1.get_width()//2, SCREEN_H//2))
+    screen.blit(paused_msg2, (SCREEN_W//2 - paused_msg2.get_width()//2, SCREEN_H//2 + 50))
 
 
 if __name__ == "__main__":
