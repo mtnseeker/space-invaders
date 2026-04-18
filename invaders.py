24d6d63bc6b37e152362ab8faa25a4cc9cecb567
Kevin Wohlwend <kevin.wohlwend@gmail.com>
2026-04-18 11:36:25 -0600

    Initialize pygame in the main function

--- invaders.py
+++ invaders.py
@@ -266,6 +266,7 @@ def pick_shooter(enemies):
 
 # --- MAIN -----------------------------------------------------------------
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
     pygame.display.set_caption("Space Invaders — Nels & Dad Edition")
     clock = pygame.time.Clock()
